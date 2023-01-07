0011a7828920fa73de4b0d9f765afb07b26b6cb4
yeongmin0 <yeong_00o@naver.com>
2023-01-08 00:40:00 +0900

    style(top bar): user select none

--- src/components/layouts/TopBar.jsx
+++ src/components/layouts/TopBar.jsx
@@ -6,7 +6,7 @@ const TopBar = (props) => {
 
   return (
     <header className="w-full h-32 border-b shadow-md grid grid-cols-6 gap-8 items-center">
-      <h1 className="text-4xl font-bold uppercase">React Sample</h1>
+      <h1 className="text-4xl font-bold uppercase select-none">React Sample</h1>
       <ul className="col-span-3 flex flex-row gap-4 font-bold text-lg">
         <Link to="/">
           <li>Home</li>
